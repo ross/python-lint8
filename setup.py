fa296b42b1c03e7f98a6c8c03c03a7d82e5005d2
Ross McFarland <rwmcfa1@gmail.com>
2011-12-24 16:10:15 -0800

    working setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,40 @@
 #!/usr/bin/env python
 
-from setuptools import setup
-from os.path import join, dirname
+from setuptools import setup, find_packages
 
-try:
-    long_description = open(join(dirname(__file__), 'README.rst')).read()
-except Exception:
-    long_description = None
+version = '0.0.1'
+long_description = ''
+for fn in ['README.rst', 'CHANGES.txt', 'TODO.txt']:
+    try:
+        long_description += open(fn).read() + '\n\n'
+    except IOError:
+        pass
 
-setup(
-    name='lint8',
-    version='0.0.1',
-    description='',
-    author='Ross McFarland',
-    author_email='ross@gmail.com',
-    url='http://github.com/ross/python-lint8',
-
-    long_description=long_description,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: POSIX',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-
-    packages=['lint8'],
-    provides=['lint8'],
-    requires=['pep8', 'pyflakes'],
-    install_requires=['pep8', 'pyflakes'],
-)
+setup(name='lint8',
+      version=version,
+      description="Python style guide checker",
+      long_description=long_description,
+      classifiers=[],
+      keywords='lint8',
+      author='Johann C. Rocholl',
+      author_email='johann@rocholl.net',
+      url='http://github.com/ross/python-lint8',
+      license='Expat license',
+      py_modules=['lint8'],
+      namespace_packages=[],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      requires=[
+          'pep8', 
+          'pyflakes'
+      ],
+      entry_points={
+          'console_scripts': [
+              'lint8 = lint8:_main',
+              ],
+          },
+      )
