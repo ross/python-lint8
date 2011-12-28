3565f5dc31289fdaec62133a640431f275bd379d
Ross McFarland <rwmcfa1@gmail.com>
2011-12-28 08:10:58 -0800

    moving towards pyflakes layout

---
+++ lint8/scripts/lint8.py
@@ -0,0 +1,32 @@
+#
+#
+#
+
+from __future__ import absolute_import
+
+from argparse import ArgumentParser
+from lint8 import Checker
+from sys import exit, stderr
+
+def main():
+    '''
+    Parse options and run lint8 checks on Python source.
+    '''
+
+    parser = ArgumentParser(description='')
+    parser.add_argument('--ignore', default='',
+                        help='skip messages of specified types (e.g. '
+                        'L003,E201,F100)')
+    parser.add_argument('--web', '-w', default=False, action='store_true',
+                        help='enable web-centric checks, no prints, '
+                        'good logging practices, ...')
+    parser.add_argument('paths', nargs='+')
+    args = parser.parse_args()
+
+    ignores = args.ignore.split(',') if args.ignore else []
+
+    checker = Checker(ignores=ignores, web=args.web)
+    count = checker.process(args.paths)
+    for message in checker.messages:
+        print >> stderr, message
+    exit(count)
